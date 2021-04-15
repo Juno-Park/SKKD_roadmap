d0f02d6ab8fcc3d547f90a09ed10ad8b40460ca1
JUN OH PARK <74538522+Juno-Park@users.noreply.github.com>
2021-04-15 15:34:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,81 @@
-window.onload = function() {
-  let openBtns = document.getElementsByClassName('item');
-  let modals = document.getElementsByClassName('modal');
-  let closeBtns = document.getElementsByClassName('close-btn');
+//여기서 부터 모달창 관련 스크립트
 
-  // getElementById와 다르게, 변수에 ClassName으로 가져온 요소(단일객체) 를 할당하게 되면
-  // 변수에는 NodeCollection(뭔 소린지 모르겠음) 이라는 형식으로 요소들이 할당됨
-  // NodeCollection은 배열처럼(배열은 아님) 숫자 인덱스와 length속성을 가지고 있음
-  // ㅅㅂ 이거 모르고 있다가 왜 오류뜨는지도 모르고 졸라 해맸음 이런 시발
+let openBtns = document.getElementsByClassName('item');
+let modals = document.getElementsByClassName('modal');
+let closeBtns = document.getElementsByClassName('close-btn');
+let modBackground = document.getElementById('modBackground');
 
+let oBtnLen = openBtns.length;
+let cBtnLen = closeBtns.length;
 
-  let oBtnLen = openBtns.length;
-  let cBtnLen = closeBtns.length;
-
-  for(let i = 0; i<oBtnLen; i++){
-    openBtns.item(i).addEventListener('click',function(){
-      modals.item(i).classList.add('active');
-    })
-  }
-
-  for(let i = 0; i<cBtnLen; i++){
-    closeBtns.item(i).addEventListener('click',function(){
-      modals.item(i).classList.remove('active');
-    })
-  }
+for(let i = 0; i<oBtnLen; i++){
+  openBtns[i].addEventListener('click',function(){
+    modBackground.classList.add('active');
+    modals[i].classList.add('active');
+  })
 }
-// openBtns[0].addEventListener('click',function(){
-//   modBackground.classList.add('active');
-//   modals[0].classList.add('active');})
 
-// closeBtns[0].addEventListener('click',function(){
-//   modBackground.classList.remove('active');
-//   modals[0].classList.remove('active');})
-  
-// openBtns[1].addEventListener('click',function(){
-//   modBackground.classList.add('active');
-//   modals[1].classList.add('active');})
+for(let i = 0; i<cBtnLen; i++){
+  closeBtns[i].addEventListener('click',function(){
+    modBackground.classList.add('popup-close');
+    modals[i].classList.add('popup-close'); //모달창이 닫히는 애니메이션과 관련된 class 추가
+    setTimeout(function(){  //setTimeout은 설정한 지연시간 뒤에 실행되는 함수임.
+      modBackground.classList.remove('active');
+      modals[i].classList.remove('active'); 
+      modBackground.classList.remove('popup-close');
+    modals[i].classList.remove('popup-close');
+    },400) //400 = 0.4s 애니메이션이 실행되는 0.4초의 지연시간을 가진 뒤에, active와 popup-close class를 동시에 삭제 함.
+  })
+}
 
-// closeBtns[1].addEventListener('click',function(){
-//   modBackground.classList.remove('active');
-//   modals[1].classList.remove('active');})
+for(let i = 0; i<cBtnLen; i++){
+  modBackground.addEventListener('click',function(){
+    modBackground.classList.add('popup-close');
+    modals[i].classList.add('popup-close');
+    setTimeout(function(){
+      modBackground.classList.remove('active');
+      modals[i].classList.remove('active');
+      modBackground.classList.remove('popup-close');
+      modals[i].classList.remove('popup-close');
+    },400)
+  })
+}
+//여기까지 모달창 관련 스크립트
 
-// openBtns[2].addEventListener('click',function(){
-//   modBackground.classList.add('active');
-//   modals[2].classList.add('active');})
 
-// closeBtns[2].addEventListener('click',function(){
-//   modBackground.classList.remove('active');
-//   modals[2].classList.remove('active');})
+
+//여기서 부터 마우스오버 이벤트 관련 스크립트
+
+let hoverArr = [];
+let hoverEventArr = [];
+
+for(let i=1; i<=10; i++){ //hover~ 클래스를 가진 요소가 총 10개임.
+  let h = 'hover';
+  hoverArr.push(h + i);  // hoverArr = ["hover1","hover2","hover3","hover4","hover5" ... ]
+  hoverEventArr.push(document.getElementsByClassName(hoverArr[i-1]));  
+} 
+// hoverEventArr = [["hover1","hover1"],["hover2","hover2"],["hover3","hove3r"],["hover4","hover4"], ... ]
+
+let eventLen = hoverEventArr.length;
+
+for(let i=0; i<eventLen; i++){
+  let k = hoverEventArr[i];
+  for(j=0; j<2; j++){ // ["hover~,hover~"] 2개의 index 밖에 없으므로..
+    k[j].addEventListener('mouseover',function(){
+      k[0].classList.add('hover');
+      k[1].classList.add('hover');
+    })}
+}
+
+
+for(let i=0; i<eventLen; i++){
+  let k = hoverEventArr[i];
+  for(j=0; j<2; j++){
+    k[j].addEventListener('mouseout',function(){
+      k[0].classList.remove('hover');
+      k[1].classList.remove('hover');
+    })}
+}
+
+//여기까지 마우스오버 이벤트 관련 스크립트
+
